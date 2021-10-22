2c53f6c41fe37bb8b85acac408ae5b32d3d8af54
skillboost <skillboost@protonmail.com>
2021-10-22 13:43:14 +0400

    Change to Dockerfiles EXPOSE PORT and App text change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Blipi
+          Feature branch change
         </a>
       </header>
     </div>
